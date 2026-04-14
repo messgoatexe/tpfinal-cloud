5d2bd9ecbd1b0f40b5d79a0c8bfb8b5ec7e9fedb
user <noecharlesgomes@icloud.com>
2026-04-14 15:21:28 +0100

    3.E - Test end-to-end (0.5 pt)

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ const today = new Date().toLocaleDateString('fr-FR')
 </script>
 <template>
 <header>
-<p>Nom : Noe Gomes</p>
+<p>Nom : Noe Gomes branche develops</p>
 <p>Date : {{ today }}</p>
 <p>Build : {{ buildId }}</p>
 </header>
